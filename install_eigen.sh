f03ab9befa678e91cd5c5535951abab92dd0895a
Daniel Marchand <daniel.marchand@gmail.com>
2019-05-17 16:33:05 +0200

    added simple eigen installation instructions

---
+++ install_eigen.sh
@@ -0,0 +1,5 @@
+#Extremely simple script that extracts eigen to /usr/local/include
+wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.bz2
+tar -xvf 3.3.7.tar.bz2
+mv eigen-eigen-323c052e1731 /usr/local/include/Eigen
+
